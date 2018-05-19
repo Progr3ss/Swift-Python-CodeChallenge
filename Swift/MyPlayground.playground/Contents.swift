9bdee214beee7fc3a4e61e7ce97dd21c3e959b20
mchibwe01@gmail.com <gageincdev@gmail.com>
2018-05-18 18:44:12 -0700

    productExceptSelf

---
+++ Swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+for countdown in stride(from: 10, to: 0, by: -3) {
+    print("\(countdown)...")
+}
